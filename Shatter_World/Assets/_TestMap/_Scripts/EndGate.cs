c4299074ca08441c33a5564918d76fd6cb750a61
CGIDAN <daniel88marko@gmail.com>
2022-12-19 16:45:56 +0000

    added a screen for finishing the level.
    upon reaching the "end gate" the time it took to finish the level will be displayed with the option to restart the level or progress to the next one.

--- Shatter_World/Assets/_TestMap/_Scripts/EndGate.cs
+++ Shatter_World/Assets/_TestMap/_Scripts/EndGate.cs
@@ -2,15 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class EndGate : MonoBehaviour
 {
-  private void OnTriggerEnter(Collider other)
-    {
+    public GameObject winScreen;  // Drag and drop the win screen canvas in the inspector
+    public Text timeText;  // Drag and drop the text element for the time in the inspector
 
+    private void OnTriggerEnter(Collider other)
+    {
         if (other.gameObject.name == "Player")
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            // Set the time text to the time it took to complete the level
+            timeText.text = "Time: " + Time.timeSinceLevelLoad.ToString("F2");
+
+            // Enable the win screen canvas
+            winScreen.SetActive(true);
+
         }
+    }
+
+
+    // Call this method when the "Try Again" button is clicked
+    public void TryAgain()
+    {
+        // Load the current scene again
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    // Call this method when the "Next Level" button is clicked
+    public void NextLevel()
+    {
+        // Load the next scene in the build index
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
